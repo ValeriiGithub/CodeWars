4aaeeabf0a45412a225d06a220f94a4c8fe837db
Valerii <137908068+ValeriiGithub@users.noreply.github.com>
2023-11-24 17:34:38 +0300

    added a new  function

--- 4 kyu/top_3_words.py
+++ 4 kyu/top_3_words.py
@@ -5,3 +5,12 @@ def top_3_words(text):
     words = re.findall(r"[a-z']*[a-z]+[a-z']*", text.lower())
     count = Counter(words)
     return [word for word, _ in count.most_common(3)]
+
+
+def top_3_words_01(text):
+    """
+    work with errors
+    """
+    words = re.findall(r"\b\w[\w']*\b", text.lower())
+    count = Counter(words)
+    return [word for word, _ in count.most_common(3)]

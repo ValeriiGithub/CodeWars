819ad9dc1b4918b0e81578acc4eb2e3307208d6b
Valerii <137908068+ValeriiGithub@users.noreply.github.com>
2024-02-28 18:13:23 +0300

    Update top_3_words.py

--- kyu_4/top_3_words.py
+++ kyu_4/top_3_words.py
@@ -14,3 +14,17 @@ def top_3_words_01(text):
     words = re.findall(r"\b\w[\w']*\b", text.lower())
     count = Counter(words)
     return [word for word, _ in count.most_common(3)]
+
+
+def top_3_words_02(text):
+    words = text.lower().split()
+    count = {}
+    for word in words:
+        word = ''.join(c for c in word if c.isalpha() or c == "'")
+        if word:
+            if word in count:
+                count[word] += 1
+            else:
+                count[word] = 1
+    return sorted(count, key=count.get, reverse=True)[:3]
+    
